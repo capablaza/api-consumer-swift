256db7a9452b7041a4fc3e2b7f6f4f53aacb2b5e
carlos apablaza <c.apablaza.z@gmail.com>
2021-03-15 15:42:47 -0300

    Initial Commit

---
+++ ApiConsumer/ApiConsumerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiConsumerApp.swift
+//  ApiConsumer
+//
+//  Created by carlos alberto apablaza zamorano on 15-03-21.
+//
+
+import SwiftUI
+
+@main
+struct ApiConsumerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
